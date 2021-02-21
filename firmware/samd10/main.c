fa430bcbde6d037b6b38753daaec4dccbc7c3465
Le Roux Bodenstein <lerouxb@gmail.com>
2021-02-21 21:29:44 +0000

    initial test code

--- firmware/samd10/main.c
+++ firmware/samd10/main.c
@@ -5,7 +5,12 @@ int main(void)
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
 
-	/* Replace with your application code */
-	while (1) {
+	uint16_t i = 0;
+
+	dac_sync_enable_channel(&DAC_0, 0);
+
+	for (;;) {
+		dac_sync_write(&DAC_0, 0, &i, 1);
+		i = (i + 1) % 1024;
 	}
 }
